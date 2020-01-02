a0474d0470a5944423ab39065e80f61da5e0091e
Aye10032 <yeyu6_505@126.com>
2020-01-02 13:17:12 +0800

    创建相关工具类

--- src/main/java/com/aye10032/BanUtil/AyeGroup.java
+++ src/main/java/com/aye10032/BanUtil/AyeGroup.java
@@ -1,9 +1,42 @@
 package com.aye10032.BanUtil;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AyeGroup {
 
-    public AyeGroup(){
+    private long Group;
+    private Map<Long, AyeMember> memberMap = new HashMap<Long, AyeMember>();
 
+    public AyeGroup(long group) {
+        this.Group = group;
+    }
+
+    public boolean MemebrExist(long QQ) {
+        return memberMap.containsKey(QQ);
+    }
+
+    public void addMemebr(long QQ) {
+        AyeMember member = new AyeMember(QQ);
+        memberMap.put(QQ, member);
+    }
+
+    public int getMemberBandeTime(long QQ) {
+        if (!MemebrExist(QQ)) {
+            return 0;
+        } else {
+            AyeMember member = memberMap.get(QQ);
+            return member.getBanedTime();
+        }
+    }
+
+    public int getMemberBanOtherTime(long QQ) {
+        if (!MemebrExist(QQ)) {
+            return 0;
+        } else {
+            AyeMember member = memberMap.get(QQ);
+            return member.getBanOtherTime();
+        }
     }
 
 }
--- src/main/java/com/aye10032/BanUtil/BanRecord.java
+++ src/main/java/com/aye10032/BanUtil/BanRecord.java
@@ -1,13 +1,24 @@
 package com.aye10032.BanUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BanRecord {
 
-    public BanRecord(){
+    private Map<Long, AyeGroup> groupMap = new HashMap<Long, AyeGroup>();
 
+    public BanRecord() {
+
+    }
+
+    public boolean GroupExist(long fromGroup) {
+        return groupMap.containsKey(fromGroup);
+    }
+
+    public AyeGroup getGroupObject(long fromGroup) {
+        return groupMap.get(fromGroup);
     }
 
 }
