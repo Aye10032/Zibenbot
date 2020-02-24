06477e3d686e76a287703f07de09bebc6a421490
dazo6 <dazo66@126.com>
2020-02-24 11:14:01 +0800

    写了一些注释

--- src/main/java/com/aye10032/Utils/TimeUtil/TimeFlow.java
+++ src/main/java/com/aye10032/Utils/TimeUtil/TimeFlow.java
@@ -7,23 +7,29 @@ public class TimeFlow implements Runnable {
 
     TimeTaskPool pool;
     Thread thread = new Thread(this);
-    int times = 0;
 
     public TimeFlow(TimeTaskPool pool) {
         this.pool = pool;
     }
 
     public void flush() {
+        //中断线程的while循环 结束线程
         thread.interrupt();
+        //创建新的线程 由于只有一个线程 这里就不使用线程池了
         thread = new Thread(this);
         thread.start();
     }
 
     public void run() {
-        System.out.println("Thread 1 run");
         while (!Thread.currentThread().isInterrupted()) {
+            //如果没有任务 就使线程长时间休眠
             if (pool.getNextTasks().size() == 0) {
-                continue;
+                try {
+                    Thread.sleep(1111114514);
+                } catch (InterruptedException e) {
+                    System.out.println("线程刷新");
+                    break;//捕获到异常之后，执行break跳出循环。
+                }
             }
             long timeInterval = pool.nextTasks.get(0).getTiggerTime().getTime() - System.currentTimeMillis();
             try {
@@ -31,14 +37,18 @@ public class TimeFlow implements Runnable {
                     Thread.sleep(timeInterval);
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.out.println("线程刷新");
                 break;//捕获到异常之后，执行break跳出循环。
             }
 
             for (TimedTask task : pool.nextTasks) {
                 try {
-                    System.out.println("run");
+                    //依次运行任务
                     task.run();
+                    //当执行次数为0时从等待任务中删除
+                    if (task.times == 0) {
+                        pool.remove(task);
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
