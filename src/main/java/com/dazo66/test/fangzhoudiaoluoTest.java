22741eae2befacbfa2cef07cca47bc2cd1a7c449
Dazo66 <895981998@qq.com>
2020-03-02 14:50:20 +0800

    update

--- src/main/java/com/dazo66/test/fangzhoudiaoluoTest.java
+++ src/main/java/com/dazo66/test/fangzhoudiaoluoTest.java
@@ -9,10 +9,10 @@ public class fangzhoudiaoluoTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         FangZhouDiaoluoFunc func = new FangZhouDiaoluoFunc();
         func.update();
-        func.run(new CQMsg(-1, -1, -1, -1, null, ".方舟素材 糖", -1));
+        func.run(new CQMsg(-1, -1, -1, -1, null, ".方舟素材 糖 gyy yt", -1));
         func.run(new CQMsg(-1, -1, -1, -1, null, ".方舟素材 双极纳米片", -1));
 
-        func.run(new CQMsg(-1, -1, -1, -1, null, ".方舟素材 全新装置", -1));
+        func.run(new CQMsg(-1, -1, -1, -1, null, ".方舟素材 涩图", -1));
 
     }
 
