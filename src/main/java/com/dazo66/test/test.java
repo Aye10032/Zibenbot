0703b7bcdedc25f52e945e52764ee90f73a7059c
Dazo66 <895981998@qq.com>
2020-03-27 12:29:35 +0800

    fix bug

--- src/main/java/com/dazo66/test/test.java
+++ src/main/java/com/dazo66/test/test.java
@@ -1,10 +1,21 @@
 package com.dazo66.test;
 
+import com.aye10032.Utils.AyeCompile;
 import com.aye10032.Utils.BiliInfo;
 
 public class test {
 
     public static void main(String[] args) {
-        System.out.println(new BiliInfo("av114514", "").getTitle());
+        AyeCompile compile = new AyeCompile("av114514");
+        if (compile.hasAV() | compile.hasBV()) {
+                BiliInfo biliInfo;
+                if (compile.hasAV()) {
+                    biliInfo = new BiliInfo(compile.getAVString(), "");
+                } else {
+                    biliInfo = new BiliInfo(compile.getBVString(), "");
+                }
+
+            System.out.println(biliInfo.getTitle());
+        }
     }
 }
