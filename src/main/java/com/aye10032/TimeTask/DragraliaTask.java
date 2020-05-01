69b4e793cabe7230509f6a8a82ee510e6114dc8a
Dazo66 <895981998@qq.com>
2020-05-01 23:57:21 +0800

    fix bug

--- src/main/java/com/aye10032/TimeTask/DragraliaTask.java
+++ src/main/java/com/aye10032/TimeTask/DragraliaTask.java
@@ -64,7 +64,7 @@ public class DragraliaTask extends TimedTask {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 1);
         calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.SECOND, 1);
         Date date = calendar.getTime();
         setRunnable(runnable).setTimes(-1).setCycle(TimeConstant.PER_HOUR).setTiggerTime(date);
     }
