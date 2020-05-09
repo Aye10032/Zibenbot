d6b1474b1f81becacc8f8db6c5780ddd2991d036
dazo6 <dazo66@126.com>
2020-05-09 21:53:33 +0800

    fix bug

--- src/main/java/com/dazo66/test/BilibiliTest.java
+++ src/main/java/com/dazo66/test/BilibiliTest.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 public class BilibiliTest {
 
     public static void main(String[] args) throws IOException {
-        String url = "https://b23.tv/BV1j64y1M7qb";
+        /*String url = "https://b23.tv/BV1j64y1M7qb/";
         RequestConfig config = RequestConfig.custom().setConnectTimeout(50000).setConnectionRequestTimeout(10000).setSocketTimeout(50000).setRedirectsEnabled(false).build();
         //2.3调用HttpURLconn方法，用于发送或者接收
         CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(config).build();
@@ -25,9 +25,9 @@ public class BilibiliTest {
 
         System.out.println(conn.getHeaders("Location")[0].getValue());
         CloseableHttpResponse conn1 = httpClient.execute(new HttpGet(conn.getHeaders("Location")[0].getValue()));
-
+*/
         BiliFunc func = new BiliFunc(null);
-        func.run(CQMsg.getTempMsg("https://b23.tv/BV1j64y1M7qb"));
+        func.run(CQMsg.getTempMsg("https://b23.tv/BV1j64y1M7qb/"));
 
 
     }
