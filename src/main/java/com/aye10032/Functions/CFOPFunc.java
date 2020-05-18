28628073ae0c978fae08a78f01182d77b3be8b4a
“Aye10032 <yeyu6_505@126.com>
2020-05-18 15:06:45 +0800

    new CFOP FUNC

--- src/main/java/com/aye10032/Functions/CFOPFunc.java
+++ src/main/java/com/aye10032/Functions/CFOPFunc.java
@@ -23,17 +23,17 @@ public class CFOPFunc extends BaseFunc {
     }
 
     public void run(CQMsg CQmsg) {
-        if (CQmsg.msg.startsWith("CFOP")) {
+        if (CQmsg.msg.startsWith("CFOP")||CQmsg.msg.startsWith("cfop")) {
             try {
                 if (CQmsg.isTeamspealMsg()) {
                     zibenbot.replyMsg(CQmsg, "ts频道无法发图片，请从群聊或者私聊获取");
                     return;
                 }
-                if (CQmsg.msg.contains("F2L")) {
+                if (CQmsg.msg.contains("F2L")||CQmsg.msg.contains("f2l")) {
                     zibenbot.replyMsg(CQmsg, zibenbot.getCQCode().image(new File(zibenbot.appDirectory + "\\image\\CFOP2.jpg")));
-                } else if (CQmsg.msg.contains("OLL")) {
+                } else if (CQmsg.msg.contains("OLL")||CQmsg.msg.contains("oll")) {
                     zibenbot.replyMsg(CQmsg, zibenbot.getCQCode().image(new File(zibenbot.appDirectory + "\\image\\CFOP3.jpg")));
-                } else if (CQmsg.msg.contains("PLL")) {
+                } else if (CQmsg.msg.contains("PLL")||CQmsg.msg.contains("pll")) {
                     zibenbot.replyMsg(CQmsg, zibenbot.getCQCode().image(new File(zibenbot.appDirectory + "\\image\\CFOP4.jpg")));
                 } else {
                     zibenbot.replyMsg(CQmsg,
