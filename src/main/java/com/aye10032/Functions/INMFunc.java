9b92d773842e29e2da3e6ade1fb0f0d5574199d8
“Aye10032 <yeyu6_505@126.com>
2020-07-19 23:49:57 +0800

    添加淫梦语录

--- src/main/java/com/aye10032/Functions/INMFunc.java
+++ src/main/java/com/aye10032/Functions/INMFunc.java
@@ -16,9 +16,9 @@ public class INMFunc extends BaseFunc {
     public void run(CQMsg CQmsg) {
         if (CQmsg.msg.contains("目力")){
             zibenbot.replyMsg(CQmsg,zibenbot.getCQCode().record("目力.amr"));
-        }else if (CQmsg.msg.contains("sodayo")){
+        }else if (CQmsg.msg.contains("sodayo")||CQmsg.msg.equals("就是啊")||CQmsg.msg.equals("救世啊")){
             zibenbot.replyMsg(CQmsg,zibenbot.getCQCode().record("sodayo.amr"));
-        }else if (CQmsg.msg.contains("压力马斯内")){
+        }else if (CQmsg.msg.contains("压力马斯内")||CQmsg.msg.equals("确实")){
             zibenbot.replyMsg(CQmsg,zibenbot.getCQCode().record("压力马斯内.amr"));
         }
     }
